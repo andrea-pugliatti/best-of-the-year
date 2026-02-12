fdea6d317f42e2403a408237a204701f204a15c4
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-02-12 18:49:31 +0100

    add getBestMovies and getBestSongs methods in MainController

--- src/main/java/org/lessons/java/best_of_the_year/classes/Song.java
+++ src/main/java/org/lessons/java/best_of_the_year/classes/Song.java
@@ -6,6 +6,11 @@ public class Song {
     private int id;
     private String title;
 
+    public Song(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
     public int getId() {
         return id;
     }
--- src/main/java/org/lessons/java/best_of_the_year/controllers/MainController.java
+++ src/main/java/org/lessons/java/best_of_the_year/controllers/MainController.java
@@ -1,5 +1,10 @@
 package org.lessons.java.best_of_the_year.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.lessons.java.best_of_the_year.classes.Movie;
+import org.lessons.java.best_of_the_year.classes.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +18,15 @@ public class MainController {
     public String getHome(Model model) {
         model.addAttribute("name", "Andrea");
         return "index";
+    }
+
+    private ArrayList<Movie> getBestMovies() {
+        return new ArrayList<>(
+                List.of(new Movie(123, "Movie 1"), new Movie(1233, "Movie 2"), new Movie(1234, "Movie 3")));
+    }
+
+    private ArrayList<Song> getBestSongs() {
+        return new ArrayList<>(List.of(new Song(15353, "Song 1"), new Song(12, "Song 2"), new Song(3414, "Song 3")));
     }
 
 }
